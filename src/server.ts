d6e0ecddfb4fbf3c673339da5bb6217af259b660
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-09-03 02:10:44 +0600

    Civicflow just some code update

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
 import app from "./app";
-
 const PORT = process.env.PORT || 5000;
-
-const main = async () => {
+async function main() {
 	try {
 		app.listen(PORT, () => {
-			console.log(`🚀 CivicFlow server is running on port ${PORT}`);
+			console.log(`Server is running on port ${PORT}`);
 		});
 	} catch (error) {
-		console.error("❌ Error starting the server:", error);
+		console.error("Error starting the server:", error);
 		process.exit(1);
 	}
-};
+}
 
 main();
