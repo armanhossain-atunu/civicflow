417259b2485645339ce941de6c929f865e27fddd
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-09-05 01:32:21 +0600

    user ragisterUser and login and get me api done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
+/** biome-ignore-all lint/style/useImportType: <explanation> */
+/** biome-ignore-all lint/correctness/noUnusedFunctionParameters: <explanation> */
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import config from "./app/config";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
+import { AuthRoutes } from "./app/module/auth/auth.route";
 
 const app: Application = express();
+app.use(
+	cors({
+		origin: config.frontend_url,
+		credentials: true,
+	}),
+);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/api/v1", AuthRoutes);
 app.get("/", (req: Request, res: Response) => {
 	res.send("Welcome to the CivicFlow app!");
 });
+
+app.use(globalErrorHandler)
+app.use(notFound)
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import app from "./app";
-const PORT = process.env.PORT || 5000;
+import { prisma } from "./app/lib/prisma";
+
+const PORT = process.env.PORT;
 async function main() {
 	try {
+		await prisma.$connect();
+		console.log("Connected to the database successfully.");
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
 		});
