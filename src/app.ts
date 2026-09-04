78c7d92082ad10312f57c25b3c9ea946d0001720
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-09-05 02:02:02 +0600

    just user api update

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/api/v1", AuthRoutes);
+app.use("/api/v1/auth", AuthRoutes);
 app.get("/", (req: Request, res: Response) => {
 	res.send("Welcome to the CivicFlow app!");
 });
